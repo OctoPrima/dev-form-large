607d1abebf0f1331e2697c53f11501d30a8223c7
OctoPrima <98483991+OctoPrima@users.noreply.github.com>
2024-10-09 19:13:33 +0300

    form

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,1 +1,16 @@
-  
+
+
+$(document).ready(function() {
+  $('select').niceSelect();
+  $('[type=tel]').mask('+00(000)00-00-000');
+
+  $( "[data-calendar]" ).wrap( "<div class='calendar-cotainer'></div>" );
+
+  $( '[data-calendar]' ).datepicker({
+    showOn: "button",
+    buttonImage: "assets/img/calendar-icon.svg",
+    buttonImageObly: true
+  });
+});
+
+
